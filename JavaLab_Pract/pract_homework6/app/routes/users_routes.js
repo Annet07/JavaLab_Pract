c27f43fbe02d25469f37c24028ed2a4e5c2722d9
Annet07 <BudrevichAnya@gmail.com>
2020-07-19 22:57:26 +0300

    homework8

--- JavaLab_Pract/pract_homework6/app/routes/users_routes.js
+++ JavaLab_Pract/pract_homework6/app/routes/users_routes.js
@@ -1,3 +1,8 @@
+const pg = require('pg');
+const config = {
+    user: 'postgres', database: 'homework8', password: '07072001', port: 5432
+};
+const pool = new pg.Pool(config);
 module.exports = function (app) {
     app.get('/users', (request, response) => {
         const result = [{
@@ -10,6 +15,37 @@ module.exports = function (app) {
                     "<p>Сбылась моя мечта!Я попала в JavaLab!!!И теперь я счааааастлива:D</p>" +
                     "<p>Люблю програмировать,а программировать не очень любит меня;)</p>" 
         }];
+        response.setHeader("Content-Type", "application/JSON");
         response.send(JSON.stringify(result));
     });
+    app.get('/love', function(request, response, next) {
+        pool.connect(function(error, client, done){
+            if(error){
+                return next(error)
+            }
+            client.query('select * from users', [], function(err, result){
+                done()
+                if(err){
+                    return next(err);
+                }
+                response.json(result.rows)
+            })
+        })
+    })
+    app.post('/love', function (req, res, next) {
+        const user = req.body;
+        pool.connect(function (err, client, done) {
+            if (err) {
+                return next(err)
+            }
+            client.query('INSERT INTO users (name, email, surname) VALUES ($1, $2, $3);', [user.name, user.email, user.surname], function (err, result) {
+                done()
+                if (err) {
+                    return next(err)
+                }
+                res.sendStatus(200)
+            })
+        })
+    });
+    
 }
